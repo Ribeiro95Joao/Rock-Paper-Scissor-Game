e8fbf2a4304d9b6d9ddd6340362229fe343b67c5
Ribeiro95Joao <ribeiro95joao@gmail.com>
2023-05-30 20:16:50 -0300

    a

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,15 +1,43 @@
 package game;
 
-import Gamers.CPUGamer;
-import Gamers.PlayerGamer;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CPUGamer cpu = new CPUGamer();
-        PlayerGamer player = new PlayerGamer();
+        int cpuWins = 0;
+        int playerWins = 0;
+       Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        while (true){
+            //CPU Plays
+            String[] cpuOptions = {"Rock", "Paper", "Scissor"};
+            int randomIndex = random.nextInt(3);
+            String cpuChoice = cpuOptions[randomIndex];
 
-        player.playerChoose();
-        cpu.cpuPlays();
+            //Player Plays
+            System.out.println("Choose your next move: R for Rock, P for Paper or S for Scissor");
+            String playerOption = scan.next();
+
+            //Validate and Results
+            if(playerOption.equals("R") || playerOption.equals("ROCK")
+                    || playerOption.equals("rock") || playerOption.equals("Rock") || playerOption.equals("r")){
+                if(cpuChoice.equals("Rock")){
+                    System.out.println("CPU chooses Rock too!");
+                    System.out.println("There's a draw!");
+                }else if(cpuChoice.equals("Paper")){
+                    System.out.println("CPU chooses Paper!");
+                    System.out.println("Paper wraps Rock! CPU won!");
+                    cpuWins++;
+                }else{
+                    System.out.println("CPU chooses Scissor!");
+                    System.out.println("Rock broke scissor! Player won!");
+                    playerWins++;
+                }
+            }
+
+        }
 
     }
+
 }
