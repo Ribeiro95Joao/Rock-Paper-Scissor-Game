71daae0ddabe13b3d5c76aa79345b1835cd9e9df
Ribeiro95Joao <ribeiro95joao@gmail.com>
2023-05-30 20:30:27 -0300

    sa

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static void main(String[] args) {
         int cpuWins = 0;
         int playerWins = 0;
-       Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         Random random = new Random();
-        while (true){
+        boolean play = true;
+        while (play == true){
             //CPU Plays
             String[] cpuOptions = {"Rock", "Paper", "Scissor"};
             int randomIndex = random.nextInt(3);
@@ -31,9 +32,49 @@ public class Main {
                     cpuWins++;
                 }else{
                     System.out.println("CPU chooses Scissor!");
-                    System.out.println("Rock broke scissor! Player won!");
+                    System.out.println("Rock broke Scissor! Player won!");
                     playerWins++;
                 }
+            }else if(playerOption.equals("P") || playerOption.equals("PAPER")
+                    || playerOption.equals("paper") || playerOption.equals("Paper") || playerOption.equals("p")){
+                if(cpuChoice.equals("Rock")){
+                    System.out.println("CPU chooses Rock!");
+                    System.out.println("Paper wraps Rock! Player won!");
+                    playerWins++;
+                }else if(cpuChoice.equals("Paper")){
+                    System.out.println("CPU chooses Paper too!");
+                    System.out.println("There's a draw!");
+                }else{
+                    System.out.println("CPU chooses Scissor!");
+                    System.out.println("Scissor cut Paper! CPU won!");
+                    cpuWins++;
+                }
+            }else if(playerOption.equals("S") || playerOption.equals("SCISSOR")
+                    || playerOption.equals("scissor") || playerOption.equals("Scissor") || playerOption.equals("s")){
+                if(cpuChoice.equals("Rock")){
+                    System.out.println("CPU chooses Rock!");
+                    System.out.println("Rock broke Scissor! Player won!");
+                    cpuWins++;
+                }else if(cpuChoice.equals("Paper")){
+                    System.out.println("CPU chooses Paper!");
+                    System.out.println("Scissor cut Paper! Player won!");
+                    playerWins++;
+                }else{
+                    System.out.println("CPU chooses Scissor too!");
+                    System.out.println("There's a draw!");
+                }
+            }else{
+                System.out.println("Invalid Option!!");
+            }
+            System.out.println("Results:");
+            System.out.println("Player's victories: " + playerWins);
+            System.out.println("CPU's victories: " + cpuWins);
+            //Continue the game or not
+            System.out.println("Do you want to play again? Y - yes or N - no");
+            String playAgain = scan.next();
+            if(playAgain.equals("N") || playAgain.equals("n") || playAgain.equals("No") ||
+                    playAgain.equals("no") || playAgain.equals("NO") ){
+                play = false;
             }
 
         }
